612b5bc3a5c442b7da1fe4d9c8674ce7079f371f
anonewb <anonewbe@gmail.com>
2019-01-12 16:59:09 +0530

    working with props

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Contact />
+        <Header branding="Contact Manager" />
+        <Contact name="John Doe" email="jdoe@gmail.com" phone="8888888888" />
+        <Contact name="Dave Smith" email="dave@gmail.com" phone="7777777777" />
       </div>
     );
   }
