53bce8e8bbc9400ccfad75442b15c39b232309e8
anonewb <anonewbe@gmail.com>
2019-01-12 16:22:37 +0530

    first class based component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Contact from "./components/Contact";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>App Component</h1>
+        <Contact />
       </div>
     );
   }
