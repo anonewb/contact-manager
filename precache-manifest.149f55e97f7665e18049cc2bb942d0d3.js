146b437e97cb54447b22de040a534abb0751c650
anonewb <anonewbe@gmail.com>
2019-01-18 22:10:05 +0530

    Updates

---
+++ precache-manifest.149f55e97f7665e18049cc2bb942d0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "9da96611ea1caa481b81",
+    "url": "/contact-manager/static/js/main.9da96611.chunk.js"
+  },
+  {
+    "revision": "f6a321842d3032097b89",
+    "url": "/contact-manager/static/js/1.f6a32184.chunk.js"
+  },
+  {
+    "revision": "9da96611ea1caa481b81",
+    "url": "/contact-manager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "f6a321842d3032097b89",
+    "url": "/contact-manager/static/css/1.b8fed66d.chunk.css"
+  },
+  {
+    "revision": "4b77bcdaf3a4cad8c680ff3fe04d0533",
+    "url": "/contact-manager/index.html"
+  }
+];
