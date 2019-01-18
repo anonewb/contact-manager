69ae5402a595a1fdd655857e24b26fc908937313
anonewb <anonewbe@gmail.com>
2019-01-18 12:47:31 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Header from "./components/layout/Header";
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
+import About from "./components/pages/About";
 
 import { Provider } from "./context";
 
@@ -12,14 +15,19 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div className="App">
-          {/* branding is passed as props for Header compo */}
-          <Header branding="Contact Manager" />
-          <div className="container">
-            <AddContact />
-            <Contacts />
+        <Router>
+          <div className="App">
+            {/* branding is passed as props for Header compo */}
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
           </div>
-        </div>
+        </Router>
       </Provider>
     );
   }
